1f0bba093c692415129ff702d8fc69b449ed0f5d
Vishal Yadav <vishuydv991@gmail.com>
2026-03-20 23:29:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    public int[][] minAbsDiff(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int resM = m - k + 1;
+        int resN = n - k + 1;
+        int[][] ans = new int[resM][resN];
+
+        for (int i = 0; i < resM; i++) {
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            
+            for (int r = i; r < i + k; r++) {
+                for (int c = 0; c < k; c++) {
+                    map.put(grid[r][c], map.getOrDefault(grid[r][c], 0) + 1);
+                }
+            }
+            
+            ans[i][0] = calculateMin(map);
+
+            for (int j = 1; j < resN; j++) {
+                
+                for (int r = i; r < i + k; r++) {
+                    int out = grid[r][j - 1];
+                    int count = map.get(out);
+                    if (count == 1) map.remove(out);
+                    else map.put(out, count - 1);
+                }
+                
+                for (int r = i; r < i + k; r++) {
+                    int in = grid[r][j + k - 1];
+                    map.put(in, map.getOrDefault(in, 0) + 1);
+                }
+                
+                ans[i][j] = calculateMin(map);
+            }
+        }
+        return ans;
+    }
+
+    private int calculateMin(TreeMap<Integer, Integer> map) {
+        int minDiff = Integer.MAX_VALUE;
+        Integer prev = null;
+        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) return 0;
+            
+            int curr = entry.getKey();
+            if (prev != null) {
+                minDiff = Math.min(minDiff, curr - prev);
+            }
+            prev = curr;
+            
+            if (minDiff == 1) return 1;
+        }
+        
+        return (minDiff == Integer.MAX_VALUE) ? 0 : minDiff;
+    }
+}
