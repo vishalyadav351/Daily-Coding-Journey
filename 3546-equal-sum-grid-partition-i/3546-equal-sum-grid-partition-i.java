e6735e9f21ea4d67e76b37a6ad70b10f88a93819
Vishal Yadav <vishuydv991@gmail.com>
2026-03-25 22:24:48 +0530

    Time: 5 ms (47.55%), Space: 160.5 MB (74.83%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -1,38 +1,38 @@
 class Solution {
-    public boolean canPartition(int[][] grid) {
+    public boolean canPartitionGrid(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         
         long totalSum = 0;
         long[] rowSums = new long[m];
-        long[] colSums = new long[n];
-        
-        for (int i = 0; i < m; i++) {
+                for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 totalSum += grid[i][j];
                 rowSums[i] += grid[i][j];
-                colSums[j] += grid[i][j];
             }
         }
-       
-        if (totalSum % 2 != 0) {
+                if (totalSum % 2 != 0) {
             return false;
         }
         
         long target = totalSum / 2;
-        
-       
-        long horizontalAccumulator = 0;
-        for (int i = 0; i < m - 1; i++) {  
+                long horizontalAccumulator = 0;
+        for (int i = 0; i < m - 1; i++) { 
             horizontalAccumulator += rowSums[i];
             if (horizontalAccumulator == target) {
                 return true;
             }
         }
+     
+        long[] colSums = new long[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                colSums[j] += grid[i][j];
+            }
+        }
         
-       
         long verticalAccumulator = 0;
-        for (int j = 0; j < n - 1; j++) {  
+        for (int j = 0; j < n - 1; j++) {
             verticalAccumulator += colSums[j];
             if (verticalAccumulator == target) {
                 return true;
