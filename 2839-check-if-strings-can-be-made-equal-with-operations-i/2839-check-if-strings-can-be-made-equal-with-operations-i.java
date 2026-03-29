a6c4bf7b9ce3cb87e978c5d311621de82470abc1
Vishal Yadav <vishuydv991@gmail.com>
2026-03-29 20:50:12 +0530

    Time: 1 ms (100%), Space: 44.9 MB (7.65%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+     
+        boolean evenMatch = (s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+                            (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0));
+        
+        boolean oddMatch = (s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+                           (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1));
+        
+        return evenMatch && oddMatch;
+    }
+}
