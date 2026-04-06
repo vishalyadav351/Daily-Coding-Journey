a1c84842beb68042278e4bedb7c1e6959429ea43
Vishal Yadav <vishuydv991@gmail.com>
2026-04-06 22:28:29 +0530

    Time: 12 ms (97.83%), Space: 50.3 MB (87.68%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.java
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -1,23 +1,48 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        
-        Set<String> obstacleSet = new HashSet<>();
+       
+        Set<Long> blockedPaths = new HashSet<>();
         for (int[] obs : obstacles) {
-            obstacleSet.add(obs[0] + "," + obs[1]);
+            long ox = (long) obs[0] + 30000;
+            long oy = (long) obs[1] + 30000;
+            blockedPaths.add((ox << 16) | oy);
         }
-        int x = 0, y = 0; 
-        int d = 0;       
-        int maxDist = 0;
+
+        int[][] moveSteps = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         
-        for (int cmd : commands) {
-            if (cmd == -1) {
-                d = (d + 1) % 4;
-            } else if (cmd == -2) {
-                d = (d + 3) % 4;
+        int posX = 0, posY = 0;
+        int currentDir = 0; 
+        int bestDistanceSq = 0;
+
+        for (int action : commands) {
+            if (action == -1) {
+                currentDir = (currentDir + 1) % 4;
+            } else if (action == -2) {
+                currentDir = (currentDir + 3) % 4;
             } else {
-                for (int i = 0; i < cmd; i++) {
-                    int nextX = x + directions[d][0];
-                    int nextY = y + directions[d][1];
+                for (int step = 0; step < action; step++) {
+                    int nextX = posX + moveSteps[currentDir][0];
+                    int nextY = posY + moveSteps[currentDir][1];
+                    
+                    long nextPosKey = ((long) nextX + 30000 << 16) | ((long) nextY + 30000);
+                    
+                    if (blockedPaths.contains(nextPosKey)) {
+                        break;
+                    }
+                    
+                    posX = nextX;
+                    posY = nextY;
+                    
+                    int currentDistSq = posX * posX + posY * posY;
+                    if (currentDistSq > bestDistanceSq) {
+                        bestDistanceSq = currentDistSq;
+                    }
+                }
+            }
+        }
+        
+        return bestDistanceSq;
+    }
+}
